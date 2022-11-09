ac280252ea4155c4e0125322f1a0bc06b543bf24
MasonPaddie <masonpaddie29@yahoo.com>
2022-11-08 19:24:08 -0600

    inital py-script set up

--- index.py
+++ index.py
@@ -1,4 +1,23 @@
-from browser import document
 
 
-document <= "Hello"
+def foo():
+        print("try this")
+    # import numpy 
+    # from stl import mesh
+    # from mpl_toolkits import mplot3d
+    # from matplotlib import pyplot
+
+    # # Create a new plot
+    # figure = pyplot.figure()
+    # axes = mplot3d.Axes3D(figure)
+
+    # # Load the STL files and add the vectors to the plot
+    # # your_mesh = mesh.Mesh.from_file('C:/home/masonp/Desktop/sei-cosmos/projects/ChunkSlice-FrontEnd/models/Spongebobs_House_v2.stl')
+    # # axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+
+    # # # Auto scale to the mesh size
+    # # scale = your_mesh.points.flatten()
+    # # axes.auto_scale_xyz(scale, scale, scale)
+
+    # # # Show the plot to the screen
+    # # pyplot.show()
